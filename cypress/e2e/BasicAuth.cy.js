c57c7784bf51b26ced268203c4de7ed77d532e91
Ben Roberts <roberts.b93@gmail.com>
2025-08-06 15:11:35 +0100

    Basic Auth - First test run, remove npm build/run stages from CI

---
+++ cypress/e2e/BasicAuth.cy.js
@@ -0,0 +1,40 @@
+describe('Basic Auth ', () =>
+{
+    const baseURL = 'the-internet.herokuapp.com/basic_auth';
+    const username = 'admin';
+    const password = 'admin';
+
+    it('verifyAuthSuccessViaAuth', () =>
+      {
+        cy.visit(`https://${baseURL}`, {
+          auth: {
+            username,
+            password,
+          }
+        });
+
+        cy.get('p').contains('Congratulations! You must have the proper credentials.');
+    });
+
+    it('verifyAuthSuccessViaDirectURL', () =>
+      {
+        const authURL = `https://${username}:${password}@${baseURL}`;
+        cy.visit(authURL);
+
+        cy.get('p').contains('Congratulations! You must have the proper credentials.');
+    });
+
+    it('verifyInvalidAuth', () =>
+      {
+        cy.request({
+          url: `https://${baseURL}`,
+          auth: {
+          username: 'wronguser',
+          password: 'wrongpass',
+        },
+        failOnStatusCode: false
+        }).then((response) => {
+          expect(response.status).to.eq(401);
+      })
+    })
+})
