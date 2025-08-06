2a2274ef387335f0a01ee3b3810a303b9d9117bc
Ben Roberts <roberts.b93@gmail.com>
2025-08-06 16:51:09 +0100

    Add remaining tests to Add Remove elements

--- cypress/e2e/AddRemoveElements.cy.js
+++ cypress/e2e/AddRemoveElements.cy.js
@@ -10,7 +10,82 @@ describe('Add Remove Elements', () =>
     {
         AddElement();
 
-        cy.get("button[onclick='deleteElement()']").should('be.visible');
+        cy.get("button[onclick='deleteElement()']")
+        .should('be.visible');
+    });
+
+    it('verifyAddMultipleElement', () => 
+    {
+        const elementsToAdd = 10;
+
+        for(let i = 0; i < elementsToAdd; i++)
+        {
+            AddElement();
+        }
+       
+        cy.get("button[onclick='deleteElement()']")
+        .should('have.length', elementsToAdd)
+    });
+
+    it('verifyDeleteElement', () => 
+    {
+        AddElement();
+
+        cy.get("button[onclick='deleteElement()']")
+        .should('have.length', 1)
+        .first()
+        .click()
+
+        cy.get("button[onclick='deleteElement()']")
+        .should('have.length', 0)
+    });
+
+    it('verifyDeleteAllElements', () => 
+    {
+        const elementsToAdd = 10;
+
+        for(let i = 0; i < elementsToAdd; i++)
+        {
+            AddElement();
+        }
+
+        cy.get("button[onclick='deleteElement()']")
+        .should('have.length', elementsToAdd)
+        .each(($btn) => 
+        {
+            cy.wrap($btn).click();
+        })
+
+         cy.get("button[onclick='deleteElement()']")
+            .should('have.length', 0)
+    });
+
+    it('verifyDeleteRandomElements', () => 
+    {
+        const elementsToAdd = 10;
+        const elementsToDelete = 3;
+
+        for(let i = 0; i < elementsToAdd; i++)
+        {
+            AddElement();
+        }
+
+        for (let i = 0; i < elementsToDelete; i++) 
+        {
+            cy.get("button[onclick='deleteElement()']")
+            .then(($buttons) => 
+            {
+                const count = $buttons.length;
+                expect(count).to.be.gte(elementsToAdd - i);
+
+                const randomIndex = Math.floor(Math.random() * count);
+
+                cy.wrap($buttons[randomIndex]).click();
+            });
+        }
+
+        cy.get("button[onclick='deleteElement()']")
+            .should('have.length', elementsToAdd - elementsToDelete)
     });
 
     const AddElement = () => 
